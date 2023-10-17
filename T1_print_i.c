2398ff92d72fa5e0c06f2edaf2b1d84e1c960932
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-16 23:43:03 -0700

    task 1 INT_MIN handle

--- T1_print_i.c
+++ T1_print_i.c
@@ -8,7 +8,7 @@
 
 int T1_print_i(va_list print_args)
 {
-	int num, numtest, l = 0, i, ml = 0, numdec;
+	int num, l = 0, i, ml = 0, numdec, numtest;
 	char *charnum;
 
 	num = va_arg(print_args, int);
@@ -16,26 +16,33 @@ int T1_print_i(va_list print_args)
 	{
 		write(1, "0", 1);
 		return (1);
+	}
+	if (num == INT_MIN)
+	{
+		write(1, "-2147483648", 11);
+		return (11);
+	}
+	if (num < 0)
+	{
+		write(1, "-", 1), ml++;
+		num = -num;
 	} numtest = num;
 	while (numtest != 0)
 	{
 		numtest /= 10;
 		l++;
-	} charnum = (char *)malloc(l + 1);
+	}
+	charnum = (char *)malloc(l + 1);
 	numtest = num;
 	i = l - 1;
-	if (numtest < 0)
-	{
-		write(1, "-", 1), ml++;
-		numtest = -numtest;
-	}
 	while (numtest > 0)
 	{
 		numdec = numtest % 10;
 		charnum[i] = numdec + '0';
 		numtest /= 10;
 		i--;
-	} charnum[l] = '\0';
+	}
+	charnum[l] = '\0';
 	write(1, charnum, l);
 	free(charnum);
 	return (l + ml);
