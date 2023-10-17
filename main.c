6c1f58f7db714c0a1e2ecb13ee27db7c14156a00
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-16 22:11:22 -0700

    task 1 errors test

--- main.c
+++ main.c
@@ -10,22 +10,11 @@
 
 int main(void)
 {
-    int len;
-    int len2;
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "NULL");
-    printf("String:[%s]\n", "NULL");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    return (0);
+	int i, l_, l;
+	i = 0;
+	l_ = _printf("i=%i\n",i);
+	l = printf("i=%i\n",i);
+	_printf("l=%i\nl_=%i\n",l,l_);
+	printf("l=%i\nl_=%i\n",l,l_);
+	return (0);
 }
